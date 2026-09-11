f22b3e03ba5ecd49ebd220674a36833511f1a2ff
smanoffmothman <smanoff.mothman@gmail.com>
2026-09-11 22:44:01 +0300

    Add files via upload

--- data/glossary-data.js
+++ data/glossary-data.js
@@ -19,5 +19,18 @@ const GLOSSARY = [
   {de: "das Oktoberfest", uk: "Октоберфест (пивний фестиваль у Мюнхені)", klas: "7", modul: "A", lektion: 1},
   {de: "die Amtssprache", uk: "офіційна (державна) мова", klas: "7", modul: "A", lektion: 1},
   {de: "liegen", uk: "розташовуватися, лежати", klas: "7", modul: "A", lektion: 1},
-  {de: "der Steckbrief", uk: "коротка розповідь про себе (анкета)", klas: "7", modul: "A", lektion: 1}
+  {de: "der Steckbrief", uk: "коротка розповідь про себе (анкета)", klas: "7", modul: "A", lektion: 1},
+  {de: "der Sommer", uk: "літо", klas: "7", modul: "A", lektion: 2},
+  {de: "das Ziel", uk: "мета, ціль", klas: "7", modul: "A", lektion: 2},
+  {de: "das Schuljahr", uk: "навчальний рік", klas: "7", modul: "A", lektion: 2},
+  {de: "packen", uk: "пакувати (валізу)", klas: "7", modul: "A", lektion: 2},
+  {de: "der Koffer", uk: "валіза", klas: "7", modul: "A", lektion: 2},
+  {de: "die Sonnencreme", uk: "сонцезахисний крем", klas: "7", modul: "A", lektion: 2},
+  {de: "der Badeanzug", uk: "купальник", klas: "7", modul: "A", lektion: 2},
+  {de: "glücklich", uk: "щасливий", klas: "7", modul: "A", lektion: 2},
+  {de: "wollen", uk: "хотіти (модальне дієслово)", klas: "7", modul: "A", lektion: 2},
+  {de: "erreichen", uk: "досягати (мети)", klas: "7", modul: "A", lektion: 2},
+  {de: "die Ferien", uk: "канікули", klas: "7", modul: "A", lektion: 2},
+  {de: "die Postkarte", uk: "листівка", klas: "7", modul: "A", lektion: 2},
+  {de: "sich freuen auf (+Akk.)", uk: "радіти (чомусь наперед), чекати на щось із задоволенням", klas: "7", modul: "A", lektion: 2}
 ];
