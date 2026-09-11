9d94db85f1403157ed3654d8996cb6ea14ed9bc1
smanoffmothman <smanoff.mothman@gmail.com>
2026-09-11 11:10:21 +0300

    Add files via upload

--- data/glossary-data.js
+++ data/glossary-data.js
@@ -14,5 +14,16 @@ const GLOSSARY = [
   {de: "verbringen", uk: "проводити (час)", klas: "7", modul: "A", lektion: 1},
   {de: "die Ferien (Pl.)", uk: "канікули", klas: "7", modul: "A", lektion: 1},
   {de: "das Ausland", uk: "закордон", klas: "7", modul: "A", lektion: 1},
-  {de: "der Großeltern (Pl.)", uk: "бабуся й дідусь", klas: "7", modul: "A", lektion: 1}
+  {de: "der Großeltern (Pl.)", uk: "бабуся й дідусь", klas: "7", modul: "A", lektion: 1},
+
+  {de: "das Nachbarland (die Nachbarländer)", uk: "сусідня країна", klas: "7", modul: "A", lektion: 1},
+  {de: "der Berg", uk: "гора", klas: "7", modul: "A", lektion: 1},
+  {de: "der Fluss", uk: "річка", klas: "7", modul: "A", lektion: 1},
+  {de: "die Hauptstadt", uk: "столиця", klas: "7", modul: "A", lektion: 1},
+  {de: "das Bundesland (die Bundesländer)", uk: "федеральна земля", klas: "7", modul: "A", lektion: 1},
+  {de: "die Speise", uk: "страва, їжа", klas: "7", modul: "A", lektion: 1},
+  {de: "das Fest", uk: "свято", klas: "7", modul: "A", lektion: 1},
+  {de: "berühmt", uk: "відомий, знаменитий", klas: "7", modul: "A", lektion: 1},
+  {de: "die Amtssprache", uk: "державна (офіційна) мова", klas: "7", modul: "A", lektion: 1},
+  {de: "feiern", uk: "святкувати", klas: "7", modul: "A", lektion: 1}
 ];
